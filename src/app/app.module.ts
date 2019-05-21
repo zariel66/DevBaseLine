1c3c65034f842289799ecd9449f0067bff86952c
Dimitri <dimitri.laaz.1990@gmail.com>
2019-05-20 21:14:09 -0500

    frontend Baseline login snippet/routing/modules/ngx-bootstap module

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { TemplateModule } from './template/template.module';
+import {ButtonsModule,BsDatepickerModule} from 'ngx-bootstrap';
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    TemplateModule,
+    ButtonsModule.forRoot(),
+    BsDatepickerModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
