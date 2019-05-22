d3ecc40f83f22df957edd553b34d6312fb578225
Dimitri <dimitri.laaz.1990@gmail.com>
2019-05-21 21:50:16 -0500

    two way binding login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -16,7 +16,8 @@ import {ButtonsModule,BsDatepickerModule} from 'ngx-bootstrap';
     AppRoutingModule,
     TemplateModule,
     ButtonsModule.forRoot(),
-    BsDatepickerModule.forRoot()
+    BsDatepickerModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
